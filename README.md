4d43097a4c017150f4d412baac7e6afbdadb488e
SeanDictionary <840851185@qq.com>
2024-12-06 17:48:59 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # NJUPT-wifi
+
+It's a tool to login NJUPT wifi.
+
+## How to use
+
+Download the realse edition and double-click to run the file.
+
+If it's the first time to run the file.It will generate a config file `account.json` under the current path.Remember to change the details in config file.
+
+When you move the file,remember to move the config together.
+
+After setting sonfig,run the file will try to connect NJUPT wifi.
+
+## Setting
+
+the account.json will as the follow.
+
+```
+{
+    "account": "account",
+    "mode": "mode",
+    "password": "password"
+}
+```
+
+Replace
+
+account → "your account"
+
+mode → "cmcc"(移动)/"njxy"(电信)/""(校园网)
+
+password → "your password"
+
+## Outputs
+
+The file will return outputs to check whether the connection is successful.
+
+Here are some outputs.
+
+| Outputs                                                                   |             Meanings             | Status |
+| ------------------------------------------------------------------------- | :-------------------------------: | :----: |
+| Generated account.json.Change the information.                            | complete the config and run again |   ⭕   |
+| dr1003({"result":0,"msg":"AC999","ret_code":2});                          |          have connected          |   ❌   |
+| dr1003({"result":0,"msg":"从Radius获取错误代码出现异常！","ret_code":1}); |            mode wrong            |   ❌   |
+| dr1003({"result":0,"msg":"账号或密码错误(ldap校验)","ret_code":1});       |     account or password wrong     |   ❌   |
+| dr1003({"result":1,"msg":"Portal协议认证成功！"});                        |       successfully connect       |   ✔   |
+
+## Others
+
+Welcome issues and PRs.
---
+++ wifi.py
@@ -0,0 +1,48 @@
+# pyinstaller --onefile --clean wifi.py
+#    _____ ______          _   _ _____ _____ _____ _______ _____ ____  _   _          _______     __
+#   / ____|  ____|   /\   | \ | |  __ \_   _/ ____|__   __|_   _/ __ \| \ | |   /\   |  __ \ \   / /
+#  | (___ | |__     /  \  |  \| | |  | || || |       | |    | || |  | |  \| |  /  \  | |__) \ \_/ / 
+#   \___ \|  __|   / /\ \ | . ` | |  | || || |       | |    | || |  | | . ` | / /\ \ |  _  / \   /  
+#   ____) | |____ / ____ \| |\  | |__| || || |____   | |   _| || |__| | |\  |/ ____ \| | \ \  | |   
+#  |_____/|______/_/    \_\_| \_|_____/_____\_____|  |_|  |_____\____/|_| \_/_/    \_\_|  \_\ |_| 
+# Designed by SeanDictionary
+import requests
+import json
+
+default_set = """{
+    "account": "account",
+    "mode": "mode",
+    "password": "password"
+}"""
+
+def load_json():
+    try:
+        with open('account.json', 'r') as file:
+            data = json.load(file)
+            account = data.get('account')
+            mode = "%40" + data.get('mode') if data.get('mode') else ""
+            password = data.get('password')
+        return account, mode, password
+    except FileNotFoundError:
+        with open('account.json', 'w') as file:
+            file.write(default_set)
+        print(f"Error: FileNotFoundError")
+        print(f"Generated account.json.Change the information.")
+        input("Press Enter to exit...")
+        exit()
+    except Exception as e:
+        print(f"Error: {e}")
+
+def fetch_url_content(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        print(response.text)
+    except Exception as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    account, mode, password = load_json()
+    url = f"https://p.njupt.edu.cn:802/eportal/portal/login?callback=dr1003&login_method=1&user_account=%2C0%2C{account}{mode}&user_password={password}"
+    fetch_url_content(url)
+    input("Press Enter to exit...")
